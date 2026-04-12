2fd9cde9c7ab34c7a84330d446ad51362592cad9
Dan Ballance <work@danballance.uk>
2026-04-12 09:57:34 +0100

    Allow moving by display lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Move by display lines (useful for wrapped lines in markdown, etc.)
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
