a8ed0a3fc9ac705ea32ccc3a4f4f989a430f61ab
Dan Ballance <work@danballance.uk>
2026-04-19 00:22:24 +0100

    Toggle file detail in oil with gd

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,31 @@
+local compact_columns = { "icon" }
+
+local detail_columns = {
+	"icon",
+	"permissions",
+	"size",
+	{ "mtime", format = "%Y-%m-%d %H:%M" },
+}
+
+local show_detail = true
+
 return {
-  "stevearc/oil.nvim",
-  lazy = false,
-  opts = {
-    default_file_explorer = true,
-    columns = { "icon" },
-  },
-  keys = {
-    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
-  },
+	"stevearc/oil.nvim",
+	lazy = false,
+	opts = {
+		default_file_explorer = true,
+		columns = detail_columns,
+		keymaps = {
+			["gd"] = {
+				desc = "Toggle Oil detail view",
+				callback = function()
+					show_detail = not show_detail
+					require("oil").set_columns(show_detail and detail_columns or compact_columns)
+				end,
+			},
+		},
+	},
+	keys = {
+		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+	},
 }
